c6119f4b072b1cfbb23d90caf3d6ea1acfbf72e8
Rietty <rie.juliette@gmail.com>
2019-01-04 18:56:33 -0500

    Renamed project for a better working title.

--- Hellcraft.cpp
+++ Hellcraft.cpp
@@ -9,7 +9,7 @@
 // Game Entry point.
 int main(int argc, char* argv[]) {
     // Create our game and head into the loop.
-    Game::Application myGame(1280, 720, "NEET: Never Ever Enter There", "Assets/Icon.png");
+    Game::Application myGame(1280, 720, "Hellcraft", "Assets/Icon.png");
     // Start game loop.
     myGame.applicationLoop();
     // Game over, exit after cleanup.
