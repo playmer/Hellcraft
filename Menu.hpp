86b21b368192876d0b324964b276798ee706297e
Rietty <rie.juliette@gmail.com>
2019-01-05 13:01:25 -0500

    Moved everything to Menu.hpp cause fuck templates.

--- Menu.hpp
+++ Menu.hpp
@@ -26,4 +26,35 @@ namespace Game {
             template <typename FSM> 
             void draw(FSM* p_fsm);
     };
+
+    void Menu::init() {
+
+    }
+
+    void Menu::cleanup() {
+
+    }
+
+    void Menu::pause() {
+
+    }
+
+    void Menu::resume() {
+
+    }
+
+    template <typename FSM> 
+    void Menu::handleEvents(FSM* p_fsm) {
+
+    }
+
+    template <typename FSM> 
+    void Menu::update(FSM* p_fsm) {
+
+    }
+
+    template <typename FSM> 
+    void Menu::draw(FSM* p_fsm) {
+
+    }
 }
