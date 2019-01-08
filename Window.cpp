a8cda46b31db1c74aee2b30f0737ed25b10b034e
Rietty <rie.juliette@gmail.com>
2019-01-08 15:58:23 -0500

    Split into cpp/hpp.

---
+++ Window.cpp
@@ -0,0 +1,17 @@
+// Projects Headers.
+#include "Window.hpp"
+
+namespace Engine {
+    Window::Window(int p_windowWidth, int p_windowHeight, std::string p_applicationTitle, std::string p_applicationIcon) : 
+    m_window {sf::VideoMode(p_windowWidth, p_windowHeight), p_applicationTitle }
+    {
+        // Create the SFML icon and set it.
+        m_icon.loadFromFile(p_applicationIcon);
+        m_window.setIcon(m_icon.getSize().x, m_icon.getSize().y, m_icon.getPixelsPtr());
+        Logger::log(INFO, "Created window and set icon.");
+    }
+
+    sf::RenderWindow& Window::getWindow() {
+        return m_window;
+    }
+}
--- Window.hpp
+++ Window.hpp
@@ -14,23 +14,10 @@ namespace Engine {
     class Window {
     public:
         Window(int p_windowWidth, int p_windowHeight, std::string p_applicationTitle, std::string p_applicationIcon);
-        auto& getWindow();
+        sf::RenderWindow& getWindow();
     
     private:
         sf::RenderWindow m_window;
         sf::Image m_icon;
     };
-
-    Window::Window(int p_windowWidth, int p_windowHeight, std::string p_applicationTitle, std::string p_applicationIcon) : 
-    m_window {sf::VideoMode(p_windowWidth, p_windowHeight), p_applicationTitle }
-    {
-        // Create the SFML icon and set it.
-        m_icon.loadFromFile(p_applicationIcon);
-        m_window.setIcon(m_icon.getSize().x, m_icon.getSize().y, m_icon.getPixelsPtr());
-        Logger::log(INFO, "Created window and set icon.");
-    }
-
-    auto& Window::getWindow() {
-        return m_window;
-    }
 }
