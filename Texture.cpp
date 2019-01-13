d75156b4df4eb5146ba634c98d8744089a0a648e
Rietty <rie.juliette@gmail.com>
2019-01-13 12:40:15 -0500

    Fixed up texture class constructor.

--- Texture.cpp
+++ Texture.cpp
@@ -9,7 +9,7 @@ namespace Engine {
     }
 
     Texture::Texture(const std::string& p_textureFile, int p_x, int p_y, int p_width, int p_height) {
-        if(!m_texture.loadFromFile(p_textureFile)) {
+        if(!m_texture.loadFromFile(p_textureFile, sf::Rect(p_x, p_y, p_width, p_height))) {
             Logger::log(ERROR, p_textureFile + " could not be loaded with given dimensions!");
         }
     }
