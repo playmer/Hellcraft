85fd59469f8511a19f6e9b76e996c417fb583ddc
Rietty <rie.juliette@gmail.com>
2019-01-14 09:51:59 -0500

    Partial implementation of Animation.

--- Animation.cpp
+++ Animation.cpp
@@ -2,5 +2,28 @@
 #include "Animation.hpp"
 
 namespace Engine {
-    
+    Animation::Animation() {
+
+    }
+
+    Animation::~Animation() {
+
+    }
+
+    void Animation::init(bool p_paused = false, bool p_looped = true) {
+        m_paused = p_paused;
+        m_looped = p_looped;
+    }
+
+    void Animation::setLooped(bool p_looped) {
+        m_looped = p_looped;
+    }
+
+    void Animation::pause() {
+        m_paused = true;
+    } 
+
+    void Animation::play() {
+        m_paused = false;
+    }
 }
--- Animation.hpp
+++ Animation.hpp
@@ -4,9 +4,32 @@
 Animation class to play, pause, loop, etc for sprites. Contains logic to create an animated sprite.
 */
 
+// SFML Headers.
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+// Wrapper Headers.
+#include "Clock.hpp"
+#include "Sprite.hpp"
+#include "Texture.hpp"
+
 namespace Engine {
-    class Animation {
+    // Inherit from these SFML classes so we can have a custom draw function.
+    class Animation : public sf::Drawable, public sf::Transformable {
     public:
+        Animation();
+        ~Animation();  
+        
+        // If we don't get anything input, we can assume these settings.
+        void init(bool p_paused = false, bool p_looped = true);
+        void setLooped(bool p_looped);
+        void pause();
+        void play();
+
     private:
+        bool m_paused;
+        bool m_looped; 
+        // Is an override function so we can pass this classes objects into windows.draw().
+        virtual void draw(sf::RenderTarget& p_target, sf::RenderStates& p_states);
     };
 }
